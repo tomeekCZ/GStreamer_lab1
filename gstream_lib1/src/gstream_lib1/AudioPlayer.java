ae734cd640678673e906e06208e5dec130bf5447
tomeekCZ <tomeek.nemecek@gmail.com>
2013-11-06 23:05:47 +0100

    Added working volume control (basic)

--- gstream_lib1/src/gstream_lib1/AudioPlayer.java
+++ gstream_lib1/src/gstream_lib1/AudioPlayer.java
@@ -16,16 +16,24 @@ import javax.swing.WindowConstants;
 import org.gstreamer.*;
 import org.gstreamer.elements.*;
 
+import sun.io.Converters;
+import sun.misc.JavaLangAccess;
+
 public class AudioPlayer {
 
 	static JFrame frame = null;
 	static JPanel panel = null;
 	static JLabel label_artist = null;
 	static JLabel label_song = null;
+	static JLabel volume_label = null;
 	
 	static JButton btnOpenFile = null;
 	static JButton btnPlaySong = null;
 	static JButton btnStopSong = null;
+	static JButton btnVolumeUp = null;
+	static JButton btnVolumeDown = null;
+	
+	static double volume_level = 0.5;
 	
 	static String artist = "";
 	static JFileChooser chooser;
@@ -76,8 +84,6 @@ public class AudioPlayer {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				play.setState(State.PLAYING);
-				
-				
 			}
 		});
 		
@@ -89,12 +95,35 @@ public class AudioPlayer {
 				play.setState(State.PAUSED);
 			}
 		});
+
+		btnVolumeDown = new JButton("Volume Down");
+		btnVolumeDown.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				volume_level -= 0.1;
+				play.set("volume", volume_level);
+				volume_label.setText(String.valueOf(volume_level));
+			}
+		});
 		
-		GridLayout layout = new GridLayout(5, 1);
+		btnVolumeUp = new JButton("Volume Up");
+		btnVolumeUp.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				volume_level += 0.1;
+				play.set("volume", volume_level);
+				volume_label.setText(String.valueOf(volume_level));
+			}
+		});
+		
+		GridLayout layout = new GridLayout(8, 1);
 		panel.setLayout(layout);
 		
-		label_artist = new JLabel("Label", SwingConstants.CENTER);
-		label_song = new JLabel("Label2", SwingConstants.CENTER);
+		label_artist = new JLabel("Artist", SwingConstants.CENTER);
+		label_song = new JLabel("Title", SwingConstants.CENTER);
+		volume_label = new JLabel(String.valueOf(volume_level), SwingConstants.CENTER);
 
 		frame.add(panel);
 		
@@ -103,6 +132,9 @@ public class AudioPlayer {
 		panel.add(btnStopSong);
 		panel.add(label_artist);
 		panel.add(label_song);
+		panel.add(btnVolumeUp);
+		panel.add(btnVolumeDown);
+		panel.add(volume_label);
 		
 		frame.setPreferredSize(new Dimension(200, 200));
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
