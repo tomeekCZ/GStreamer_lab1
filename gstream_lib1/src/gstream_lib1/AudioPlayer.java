100bb28528442f6471568b1f65b69d8b28e4cf24
tomeekCZ <tomeek.nemecek@gmail.com>
2013-11-06 07:52:29 +0100

    Added ability to show title and artist of song - player can now play only songs

--- gstream_lib1/src/gstream_lib1/AudioPlayer.java
+++ gstream_lib1/src/gstream_lib1/AudioPlayer.java
@@ -1,45 +1,80 @@
 package gstream_lib1;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
 
 import org.gstreamer.*;
-import org.gstreamer.swing.VideoComponent;
+import org.gstreamer.elements.*;
 
-public class VideoTest {
-	public VideoTest() {
-    }
-    private static Pipeline pipe;
-    public static void main(String[] args) {
-        args = Gst.init("VideoTest", args);
-        pipe = new Pipeline("VideoTest");
-        final Element videosrc = ElementFactory.make("videotestsrc", "source");
-        final Element videofilter = ElementFactory.make("capsfilter", "filter");
-        videofilter.setCaps(Caps.fromString("video/x-raw-yuv, width=720, height=576"
-                + ", bpp=32, depth=32, framerate=25/1"));
-        SwingUtilities.invokeLater(new Runnable() {
+public class AudioPlayer {
 
-            public void run() {
-                VideoComponent videoComponent = new VideoComponent();
-                Element videosink = videoComponent.getElement();
-                pipe.addMany(videosrc, videofilter, videosink);
-                Element.linkMany(videosrc, videofilter, videosink);
-                
-                // Now create a JFrame to display the video output
-                JFrame frame = new JFrame("Swing Video Test");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.add(videoComponent, BorderLayout.CENTER);
-                videoComponent.setPreferredSize(new Dimension(720, 576));
-                frame.pack();
-                frame.setVisible(true);
-                
-                // Start the pipeline processing
-                pipe.setState(State.PLAYING);
-            }
-        });
-    }
+	static JFrame frame = null;
+	static JPanel panel = null;
+	static JLabel label_artist = null;
+	static JLabel label_song = null;
+	static String artist = "";
+	static JFileChooser chooser;
+
+	public static void main(String[] args) {
+		args = Gst.init("AudioPlayer", args);
+
+		chooser = new JFileChooser();
+		frame = new JFrame("File Chooser");
+		panel = new JPanel();
+		
+		GridLayout layout = new GridLayout(2, 1);
+		panel.setLayout(layout);
+		
+		label_artist = new JLabel("Label", SwingConstants.CENTER);
+		label_song = new JLabel("Label2", SwingConstants.CENTER);
+
+		frame.add(panel);
+		
+		panel.add(label_artist);
+		panel.add(label_song);
+		
+		frame.setPreferredSize(new Dimension(200, 100));
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+		chooser.showOpenDialog(frame);
+
+		PlayBin2 play = new PlayBin2("bin");
+		play.setInputFile(chooser.getSelectedFile());
+		play.setVideoSink(ElementFactory.make("fakesink", "sink"));
+
+		play.getBus().connect(new Bus.TAG() {
+
+			public void tagsFound(GstObject source, TagList tagList) {
+				for (String tagName : tagList.getTagNames()) {
+					for (final Object tagData : tagList.getValues(tagName)) {
+						System.out.printf("[%s]=%s\n", tagName, tagData);
+						if (tagName.equals("artist")) {
+							label_artist.setText(tagData.toString());
+						}
+						
+						else if (tagName.equals("title")) {
+							label_song.setText(tagData.toString());
+						}
+					}
+				}
+			}
+		});
+
+		play.setState(State.PLAYING);
+		Gst.main();
+		play.setState(State.NULL);
+	}
+
 }
-
