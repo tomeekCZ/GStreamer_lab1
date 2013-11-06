f951c71947a6d9cf82cdb48a7c13e7d0a253e73a
tomeekCZ <tomeek.nemecek@gmail.com>
2013-11-06 09:17:42 +0100

    Added buttons for playing and selecting songs

--- gstream_lib1/src/gstream_lib1/AudioPlayer.java
+++ gstream_lib1/src/gstream_lib1/AudioPlayer.java
@@ -2,7 +2,10 @@ package gstream_lib1;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,17 +22,75 @@ public class AudioPlayer {
 	static JPanel panel = null;
 	static JLabel label_artist = null;
 	static JLabel label_song = null;
+	
+	static JButton btnOpenFile = null;
+	static JButton btnPlaySong = null;
+	static JButton btnStopSong = null;
+	
 	static String artist = "";
 	static JFileChooser chooser;
+	
+	static PlayBin2 play = null;
 
 	public static void main(String[] args) {
 		args = Gst.init("AudioPlayer", args);
-
+		
 		chooser = new JFileChooser();
 		frame = new JFrame("File Chooser");
 		panel = new JPanel();
 		
-		GridLayout layout = new GridLayout(2, 1);
+		btnOpenFile = new JButton("Open FIle");
+		btnOpenFile.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				chooser.showOpenDialog(frame);
+				play = null;
+				play = new PlayBin2("bin");
+				play.setInputFile(chooser.getSelectedFile());
+				play.setVideoSink(ElementFactory.make("fakesink", "sink"));
+				
+				play.getBus().connect(new Bus.TAG() {
+
+					public void tagsFound(GstObject source, TagList tagList) {
+						for (String tagName : tagList.getTagNames()) {
+							for (final Object tagData : tagList.getValues(tagName)) {
+								System.out.printf("[%s]=%s\n", tagName, tagData);
+								if (tagName.equals("artist")) {
+									label_artist.setText(tagData.toString());
+								}
+								
+								else if (tagName.equals("title")) {
+									label_song.setText(tagData.toString());
+								}
+							}
+						}
+					}
+				});
+			}
+		});
+		
+		btnPlaySong = new JButton("Play");
+		btnPlaySong.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				play.setState(State.PLAYING);
+				
+				
+			}
+		});
+		
+		btnStopSong = new JButton("Stop");
+		btnStopSong.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				play.setState(State.PAUSED);
+			}
+		});
+		
+		GridLayout layout = new GridLayout(5, 1);
 		panel.setLayout(layout);
 		
 		label_artist = new JLabel("Label", SwingConstants.CENTER);
@@ -37,40 +98,20 @@ public class AudioPlayer {
 
 		frame.add(panel);
 		
+		panel.add(btnOpenFile);
+		panel.add(btnPlaySong);
+		panel.add(btnStopSong);
 		panel.add(label_artist);
 		panel.add(label_song);
 		
-		frame.setPreferredSize(new Dimension(200, 100));
+		frame.setPreferredSize(new Dimension(200, 200));
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 
-		chooser.showOpenDialog(frame);
+		
 
-		PlayBin2 play = new PlayBin2("bin");
-		play.setInputFile(chooser.getSelectedFile());
-		play.setVideoSink(ElementFactory.make("fakesink", "sink"));
-
-		play.getBus().connect(new Bus.TAG() {
-
-			public void tagsFound(GstObject source, TagList tagList) {
-				for (String tagName : tagList.getTagNames()) {
-					for (final Object tagData : tagList.getValues(tagName)) {
-						System.out.printf("[%s]=%s\n", tagName, tagData);
-						if (tagName.equals("artist")) {
-							label_artist.setText(tagData.toString());
-						}
-						
-						else if (tagName.equals("title")) {
-							label_song.setText(tagData.toString());
-						}
-					}
-				}
-			}
-		});
-
-		play.setState(State.PLAYING);
 		Gst.main();
 		play.setState(State.NULL);
 	}
